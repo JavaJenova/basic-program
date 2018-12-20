51cbf5549c2295727f45332ee85debcfbb795e72
JavaJenova <45706568+JavaJenova@users.noreply.github.com>
2018-12-20 11:31:51 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,36 @@
+package siva;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import jeno.Calculate;
+
+public class Test {
+	
+	public static void main(String[] args) {
+//		Test t  = new Test();
+//		Scanner s = new Scanner(System.in);
+//		PrintStream out ;
+//		Object o = new Object();
+//		 t.add();
+//		Calculate c = new Calculate();
+//		
+		System.out.println(A.t);
+	}
+	
+	void add(){
+		System.out.println("ADDITION");
+	}
+
+}
+
+
+class A{
+	int a[];
+	static Test t=null;
+	static{
+		System.out.println("static block");
+	}
+
+	
+}
